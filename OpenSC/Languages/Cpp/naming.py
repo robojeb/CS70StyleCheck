7eb860d697dcedc4650588dcee7cf6fd80f0e228
Jeb Brooks <jeb@wl-194-182.wpa.claremont.edu>
2014-07-07 09:09:47 -0700

    finished basic naming functions

--- OpenSC/Languages/Cpp/naming.py
+++ OpenSC/Languages/Cpp/naming.py
@@ -67,12 +67,15 @@ def checkMemberVariables(checkFile, regex, \
           node.location.line, node.location.column, \
           title, message)
 
-  nodeSearch(c)
+    for c in node.get_children():
+      classSearch(c)
+
+  nodeSearch(checkFile.getlibclangAST().cursor)
 
 def checkStaticVariables(checkFile, regex, \
-    title="Invalid member variable name", \
+    title="Invalid static variable name", \
     message="Cunsult your style guidelines for correct static variable names"):
-  '''This function takes a regular expression which defines a correct member
+  '''This function takes a regular expression which defines a correct static
      variable name and adds an issue for each instance of a static variable that
      does not conform to this regular expression. (This function does not
      currently differentiate between constant and non-constant static variables
@@ -100,4 +103,84 @@ def checkStaticVariables(checkFile, regex, \
           node.location.line, node.location.column, \
           title, message)
 
-  nodeSearch(c)
+    for c in node.get_children():
+      classSearch(c)
+
+  nodeSearch(checkFile.getlibclangAST().cursor)
+
+def checkGlobalVariables(checkFile, regex, \
+    title="Invalid global variable name", \
+    message="Cunsult your style guidelines for correct global variable names"):
+  '''This function takes a regular expression which defines a correct global
+     variable name and adds an issue for each instance of a global variable that
+     does not conform to this regular expression. (By putting an empty regex and
+     changing the message it is possible to disallow global variables)'''
+
+  globalVariableRegex = re.compile(regex)
+
+  def nodeSearch(node):
+    if node.location.file != None and \
+        str(node.location.file) != checkFile.getBaseName():
+      return
+    if node.kind == clang.cindex.CursorKind.VAR_DECL:
+      if globalVariableRegex.match(node.spelling) is None:
+        checkFile.addLineIssue(Severity.ERROR, \
+        node.location.line, node.location.column, \
+        title, message)
+    elif node.kind == clang.cindex.CursorKind.CLASS_DECL or\
+        node.kind == clang.cindex.CursorKind.FUNCTION_DECL or\
+        node.kind == clang.cindex.CursorKind.CXX_METHOD:
+      return
+
+  nodeSearch(checkFile.getlibclangAST().cursor)
+
+def checkClassNames(checkFile, regex, \
+    title="Invalid class name", \
+    message="Cunsult your style guidelines for correct class names"):
+  '''This function takes a regular expression which defines a correct class
+     name and adds an issue for each instance of a class that
+     does not conform to this regular expression.'''
+
+  classNameRegex = re.compile(regex)
+
+  def nodeSearch(node):
+    if node.location.file != None and \
+        str(node.location.file) != checkFile.getBaseName():
+      return
+
+    if node.kind == clang.cindex.CursorKind.CLASS_DECL:
+      if classNameRegex.match(node.spelling) is None:
+        checkFile.addLineIssue(Severity.ERROR, \
+        node.location.line, node.location.column, \
+        title, message)
+
+    for c in node.get_children():
+      nodeSearch(c)
+
+  nodeSearch(checkFile.getlibclangAST().cursor)
+
+def checkFunctionNames(checkFile, regex, \
+    title="Invalid function name", \
+    message="Cunsult your style guidelines for correct function names"):
+  '''This function takes a regular expression which defines a correct function
+     name and adds an issue for each instance of a function that
+     does not conform to this regular expression.'''
+
+  functionNameRegex = re.compile(regex)
+
+  def nodeSearch(node):
+    if node.location.file != None and \
+        str(node.location.file) != checkFile.getBaseName():
+      return
+
+    if node.kind == clang.cindex.CursorKind.FUNCTION_DECL or \
+        node.kind == clang.cindex.CursorKind.CXX_METHOD:
+      if functionNameRegex.match(node.spelling) is None:
+        checkFile.addLineIssue(Severity.ERROR, \
+        node.location.line, node.location.column, \
+        title, message)
+
+    for c in node.get_children():
+      nodeSearch(c)
+
+  nodeSearch(checkFile.getlibclangAST().cursor)
