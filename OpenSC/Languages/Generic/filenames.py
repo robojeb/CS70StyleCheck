1932d76a4911c711f986fd53ce2d3e460dce131e
Jeb Brooks <jeb@wl-194-182.wpa.claremont.edu>
2014-07-07 13:45:45 -0700

    Added a filename check.

--- OpenSC/Languages/Generic/filenames.py
+++ OpenSC/Languages/Generic/filenames.py
@@ -3,6 +3,7 @@ This module is designed for checking filenames and extensions. It is useful when
 used as a check during an automatic commit hook check.
 '''
 
+import re
 from severity import Severity
 
 def checkExtension(checkFile, allowedExtensions,
@@ -10,6 +11,21 @@ def checkExtension(checkFile, allowedExtensions,
     message=\
     "Files with this extension should not be committed to the repository",\
     severity=Severity.ERROR):
+  '''This function checks that the extension to the file is in the allowed list
+     of extensions.'''
 
   if not checkFile.getExtension() in allowedExtensions:
     checkFile.addFileIssue(severity, title, message)
+
+def checkFilename(checkfile, regex,\
+    title="Filename violation",\
+    message="Check your styleguide for file naming conventions",\
+    severity=Severity.WARNING):
+  '''This function checks that the filename is valid according to a regular
+     expression provided by the script writer.'''
+
+  nameRegex = re.compile(regex)
+
+  if nameRegex.match(checkFile.getRawName()) is None:
+    checkFile.addFileIssue(severity, title, message)
+  
