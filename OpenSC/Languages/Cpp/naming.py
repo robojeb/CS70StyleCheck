3148a1398e674820e09979f8e2ea8f8c6c8cd55c
Jeb Brooks <jeb@wl-194-182.wpa.claremont.edu>
2014-07-07 08:47:35 -0700

    Worked on naming checks for C++.

--- OpenSC/Languages/Cpp/naming.py
+++ OpenSC/Languages/Cpp/naming.py
@@ -6,8 +6,8 @@ names.
 import re
 from Severity import Severity
 
-def checkLocalNames(checkFile, regex, \
-    title="Invalid Local Variable Name", \
+def checkLocalVariables(checkFile, regex, \
+    title="Invalid local variable name", \
     message="Consult your style guidelines for correct local variable names"):
   '''This function takes a regular expression which defines a correct local
      variable name and adds an issue for each instance of a local variable that
@@ -33,8 +33,71 @@ def checkLocalNames(checkFile, regex, \
     if node.kind == clang.cindex.CursorKind.VAR_DECL:
       if localVariableRegex.match(node.spelling) is None:
         checkFile.addLineIssue(Severity.ERROR, \
-          node.location.line, node.location.column,\
+          node.location.line, node.location.column, \
           title, message)
 
   nodeSearch(checkFile.getlibclangAST().cursor)
-  
+
+def checkMemberVariables(checkFile, regex, \
+    title="Invalid member variable name", \
+    message="Cunsult your style guidelines for correct member variable names"):
+  '''This function takes a regular expression which defines a correct member
+     variable name and adds an issue for each instance of a member variable that
+     does not conform to this regular expression'''
+
+  memberVariableRegex = re.compile(regex)
+
+  def nodeSearch(node):
+    if node.location.file != None and \
+        str(node.location.file) != checkFile.getBaseName():
+      return
+    if node.kind == clang.cindex.CursorKind.CLASS_DECL:
+      classSearch(node)
+    else:
+      for c in node.get_children():
+        nodeSearch(c)
+
+  def classSearch(node):
+    if node.location.file != None and \
+        str(node.location.file) != checkFile.getBaseName():
+      return
+    if node.kind == clang.cindex.CursorKind.FIELD_DECL:
+      if memberVariableRegex.match(node.spelling) is None:
+        checkFile.addLineIssue(Severity.ERROR, \
+          node.location.line, node.location.column, \
+          title, message)
+
+  nodeSearch(c)
+
+def checkStaticVariables(checkFile, regex, \
+    title="Invalid member variable name", \
+    message="Cunsult your style guidelines for correct static variable names"):
+  '''This function takes a regular expression which defines a correct member
+     variable name and adds an issue for each instance of a static variable that
+     does not conform to this regular expression. (This function does not
+     currently differentiate between constant and non-constant static variables
+     )'''
+
+  staticVariableRegex = re.compile(regex)
+
+  def nodeSearch(node):
+    if node.location.file != None and \
+        str(node.location.file) != checkFile.getBaseName():
+      return
+    if node.kind == clang.cindex.CursorKind.CLASS_DECL:
+      classSearch(node)
+    else:
+      for c in node.get_children():
+        nodeSearch(c)
+
+  def classSearch(node):
+    if node.location.file != None and \
+        str(node.location.file) != checkFile.getBaseName():
+      return
+    if node.kind == clang.cindex.CursorKind.VAR_DECL:
+      if memberVariableRegex.match(node.spelling) is None:
+        checkFile.addLineIssue(Severity.ERROR, \
+          node.location.line, node.location.column, \
+          title, message)
+
+  nodeSearch(c)
