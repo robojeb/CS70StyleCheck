7229b41d6fc9e9f79339bc0e5a132f030058918a
Jeb Brooks <jeb@wl-194-182.wpa.claremont.edu>
2014-07-09 15:44:40 -0700

    Worked on hw0 script

---
+++ HMC/CS70/generictest.py
@@ -0,0 +1,4 @@
+'''
+This file is a generic test and style check script which emails the students
+with the results of their tests
+'''
--- HMC/CS70/helpers.py
+++ HMC/CS70/helpers.py
@@ -4,3 +4,28 @@ test student submissions.
 '''
 
 import sys, os, subprocess, opensc
+
+
+def runProcess(arguments):
+  proc = subprocess.Popen(arguments, env=os.environ, stdout=subprocess.PIPE,\
+                          stderr=subprocess.PIPE)
+  proc.wait()
+  return proc.stdout.read(), proc.stderr.read()
+
+def runProcessAsync(arguments):
+  proc = subprocess.Popen(arguments, env=os.environ)
+
+#Checks for .o and no extension files except those in exceptions
+def checkForIllegalFiles(exceptions=['Makefile']):
+  #TODO:Figure out the best way to do this
+  pass
+
+def styleCheck(extensions=['cpp','hpp'], ignore=[]):
+  #TODO:Make this work
+  pass
+
+def compileCheck():
+  runProcess(['make', 'clean'])
+  stdout, stderr = runProcess(['make'])
+  runProcess(['make', 'clean'])
+  return stdout, stderr
